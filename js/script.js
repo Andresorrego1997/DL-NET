52536d0cae653f0538b604726657228de8ff40b0
unknown <Administrador@Lsistemas2.ferrosvel.local>
2023-02-04 21:39:15 -0500

    Actualización de datos

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ function open_close_menu(){
     body.classList.toggle("body_move");
     side_menu.classList.toggle("menu__side_move");
 }
+
+if (window.innerWidth < 760){
+    body.classList.add("body_move");
+    side_menu.classList.add("menu__side_move");
+}
+
+window.addEventListener("resize", function(){
+    if (window.innerWidth > 760){
+        body.classList.remove("body_move");
+        side_menu.classList.remove("menu__side_move");
+    }
+
+    if (window.innerWidth < 760){
+        body.classList.remove("body_move");
+        side_menu.classList.remove("menu__side_move");
+    }
+})
